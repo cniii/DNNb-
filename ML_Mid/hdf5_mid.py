f76a1e06be7a91f9dcbe714cdc9695f41b448389
Cheng Ni <chengni628@gmail.com>
2017-08-02 01:14:56 -0700

    update

--- ML_Mid/hdf5_mid.py
+++ ML_Mid/hdf5_mid.py
@@ -9,9 +9,11 @@ f = h5py.File('/phys/groups/tev/scratch4/users/chengni/gjj_Variables_mid.hdf5', 
 # number of input data files
 size = 10000000
 var_num = 28
+batch = 10000
+group = size / batch
 
 # Create dataset
-dset_var = f.create_dataset('mid_variables', (15, 28, size))
+dset_var = f.create_dataset('mid_variables', (1, 422, size))
 dset_pid = f.create_dataset('mid_pid', (3, size))
 
 # create groups - 15 tracks
@@ -19,28 +21,26 @@ for j in range(var_num):
     g = f.create_group('track_' + str(j))
 
 # write the data from src
-for j in range(size):
-    if j % 1000 == 0:
-        print 'loading #' + str(j)
+for j in range(group):
+    print '@group ' + str(j) + '/' + str(group)
+    mid = np.empty([batch, 422])
+    pid = np.empty([batch, 3])
+    for k in range(batch):
+        mid_tmp = np.load(filepath + '/saved_batches_test/clean_dijet_mid_' + str(j * batch + k) + '.npy')
+        y = np.load(filepath + '/saved_batches_test/clean_dijet_y_' + str(j * batch + k) + '.npy')
 
-    mid = np.empty([15, 28])
-    pid = np.empty([3])
+        mid[k, :] = mid_tmp
 
-    mid_tmp = np.load(filepath + '/saved_batches_test/clean_dijet_mid_' + str(j) + '.npy')
-    y = np.load(filepath + '/saved_batches_test/clean_dijet_y_' + str(j) + '.npy')
-
-    for k in range(15):
-        mid[k, :, :] = np.resize(np.transpose(mid_tmp[28 * k: 28 * (k + 1), [28])
-    if y == 5:
-        pid[:] = [0, 0, 1]
-    elif y == 4:
-        pid[:] = [0, 1, 0]
-    else:
-        pid[:] = [1, 0, 0]
+        if y == 5:
+            pid[k] = [0, 0, 1]
+        elif y == 4:
+            pid[k] = [0, 1, 0]
+        else:
+            pid[k] = [1, 0, 0]
 
     # put into the data set
     dset_var[:, :, j] = mid
     dset_pid[:, j] = y
 
 for j in range(15):
-    f['track_' + str(j)] = dset_var[j, :, :]
+    f['track_' + str(j)] = dset_var[:, j * 28: (j + 1) * 28, :]
